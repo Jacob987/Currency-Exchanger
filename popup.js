10d92c307ab6b89b39ee557b3f6f53a3486c32c1
Jacob Kim <jkkim1998@gmail.com>
2020-08-26 18:44:49 +1200

    Got the button to trigger an event change!

--- popup.js
+++ popup.js
@@ -4,19 +4,28 @@ var GBP_TO_NZD;
 var AUD_TO_NZD;
 let DATA;
 
+
 fetch('http://data.fixer.io/api/latest?access_key=185ab81e58e51f47a9afcb917ef171e5')
 .then(res => res.json())
 .then((data) => {
     DATA = data;
 });
 
-function calculate() {
-    const d = document.getElementById("currency");
-    const b = document.getElementById("amount");
-    let amount = b.value;
-    let cur = d.options[d.selectedIndex].text;
-    document.getElementById("outPut").innerHTML = DATA.rates.USD;
-}
+document.getElementById("button").addEventListener('click', function() {
+    document.getElementById("outPut").innerHTML="";
+    const div = document.createElement('div');
+    div.textContent = "THIS IS WORKING!!";
+    document.getElementById("outPut").appendChild(div);
+});
+
+
+// function calculate() {
+//     const d = document.getElementById("currency");
+//     const b = document.getElementById("amount");
+//     let amount = b.value;
+//     let cur = d.options[d.selectedIndex].text;
+//     alert("hello");
+// }
 
 function nzdAsBase(data) {
     USD_TO_NZD = data.rates.NZD/data.rates.USD;
