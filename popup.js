891d8676bed2b576b4ab8a900eb5bf201aa82dd7
Jacob Kim <jkkim1998@gmail.com>
2020-08-26 16:10:43 +1200

    commit

--- popup.js
+++ popup.js
@@ -1,7 +1,6 @@
-$.getJSON('http://data.fixer.io/api/latest?access_key=185ab81e58e51f47a9afcb917ef171e5', function(data) {
-    const nzd_base = data.rates.NZD;
-    const nzd_to_eur = 1/nzd_base;
-    const usd_to_eur = 1/data.rates.USD;
-    alert(usd_to_eur/nzd_to_eur);
+fetch('http://data.fixer.io/api/latest?access_key=185ab81e58e51f47a9afcb917ef171e5')
+.then(res => res.json())
+.then((data) => {
+    document.getElementById("p").innerHTML = data.rates.NZD;
 });
-alert("HELLO");
+
