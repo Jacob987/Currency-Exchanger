0269c95bc07881d8891dfe9a6d914ed512a73453
Jacob Kim <jkkim1998@gmail.com>
2020-08-26 16:13:17 +1200

    The API that shows currency exchange rate now shows on extension bar

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 fetch('http://data.fixer.io/api/latest?access_key=185ab81e58e51f47a9afcb917ef171e5')
 .then(res => res.json())
 .then((data) => {
-    document.getElementById("p").innerHTML = data.rates.NZD;
+    document.getElementById("p").innerHTML = data.rates.NZD/data.rates.USD;
 });
 
