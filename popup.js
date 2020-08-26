2df7781882135e7c601a2bbefbf63e6401494e40
Jacob Kim <jkkim1998@gmail.com>
2020-08-26 19:44:45 +1200

    Almost got it working, but I think there is an issue with the fetch API with CORS in particular

--- popup.js
+++ popup.js
@@ -8,24 +8,8 @@ let DATA;
 fetch('http://data.fixer.io/api/latest?access_key=185ab81e58e51f47a9afcb917ef171e5')
 .then(res => res.json())
 .then((data) => {
-    DATA = data;
+    nzdAsBase(data);
 });
-
-document.getElementById("button").addEventListener('click', function() {
-    document.getElementById("outPut").innerHTML="";
-    const div = document.createElement('div');
-    div.textContent = "THIS IS WORKING!!";
-    document.getElementById("outPut").appendChild(div);
-});
-
-
-// function calculate() {
-//     const d = document.getElementById("currency");
-//     const b = document.getElementById("amount");
-//     let amount = b.value;
-//     let cur = d.options[d.selectedIndex].text;
-//     alert("hello");
-// }
 
 function nzdAsBase(data) {
     USD_TO_NZD = data.rates.NZD/data.rates.USD;
@@ -33,3 +17,37 @@ function nzdAsBase(data) {
     GBP_TO_NZD = data.rates.NZD/data.rates.GBP;
     AUD_TO_NZD = data.rates.NZD/data.rates.AUD;
 }
+
+document.getElementById("button").addEventListener('click', calculate);
+document.getElementById("button").addEventListener('keyup', function(e) {
+    if (e.keyCode === 13) {
+        calculate();
+    }
+})
+
+function calculate() {
+    let newPrice;
+    const d = document.getElementById("currency");
+    const b = document.getElementById("amount");
+    let oldPrice = b.value;
+    let cur = d.options[d.selectedIndex].text;
+    if (cur == "USD") {
+        newPrice = oldPrice*USD_TO_NZD;
+    }
+    else if (cur == "EUR") {
+        newPrice = oldPrice*USD_TO_NZD;
+    }
+    else if (cur == "GBP") {
+        newPrice = oldPrice*GBP_TO_NZD;
+    }
+    else if (cur == "AUD") {
+        newPrice = oldPrice*AUD_TO_NZD;
+    }
+    document.getElementById("outPut").innerHTML="";
+    const div = document.createElement('div');
+    div.textContent = `${newPrice}`;
+    document.getElementById("outPut").appendChild(div);
+}
+
+
+
